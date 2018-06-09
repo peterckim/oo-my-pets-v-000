1b4e91e9f9efd7a352d1928d7bb453c0254e397c
Peter Kim <peterckim96@gmail.com>
2018-06-09 19:58:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+  def initialize(name, mood="nervous")
+    @name = name
+    @mood = mood
+  end
+  
+  def name
+    @name
+  end
+  
+  def mood=(mood)
+    @mood = mood
+  end
+  
+  def mood
+    @mood
+  end
 end
