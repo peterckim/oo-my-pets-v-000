6ae77e4323479f4d6f69f1bb48c7ba29c6748b84
Peter Kim <peterckim96@gmail.com>
2018-06-09 20:02:13 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -15,4 +15,14 @@ class Owner
     new_cat = Cat.new(name)
     @pets[:cats] << new_cat
   end
+  
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    @pets[:dogs] << new_dog
+  end
+  
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[:fishes] << new_fish
+  end
 end
